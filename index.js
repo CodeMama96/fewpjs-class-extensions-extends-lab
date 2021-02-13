4430aab10540037c6debf64f1ea2d6b77e6ff34e
Alicia Weenum <weenum.alicia@gmail.com>
2021-02-12 19:57:25 -0500

    commit tree d2132e1ce48ce191028fd0ebed2884b56978ed6b

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+
+class Polygon {
+    constructor(count, perimeter){
+        this.count = count
+        this.perimeter = perimeter
+
+    }
+
+    get countSides(){
+
+    }
+
+}
+
+class Square extends Polygon {
+
+}
+
+class Triangle extends Polygon{
+
+}
